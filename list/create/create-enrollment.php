858a46811a0211cd1af6c596b1986ba3d903ef72
Fred Luartes <fjluartes@gmail.com>
2017-12-01 16:31:17 +0800

    edits for enrollment

--- list/create/create-enrollment.php
+++ list/create/create-enrollment.php
@@ -23,32 +23,28 @@ session_start();
     <form action='create-enrollment.php' method='post'>
 		<table>
             <tr>
-                <td><b>Name </b></td>
-                <td><input name='name' type='text' required></td>
+                <td><b>Student Name</b></td>
+                <td>".$sid."</td>
 			</tr>
             <tr>
-                <td><b>Course</b></td>
-                <td>".$course."</td>
+                <td><b>No. of Units</b></td>
+                <td><input name='num_units' type='text'></td>
 			</tr>
             <tr>
-                <td><b>School</b></td>
-                <td>".$school."</td>
+                <td><b>School Year</b></td>
+                <td><input name='year' type='text'></td>
 			</tr>
             <tr>
-                <td><b>Birthdate</b></td>
-                <td><input name='birthdate' type='date'></td>
+                <td><b>Semester</b></td>
+                <td><input name='semester' type='text'></td>
 			</tr>
             <tr>
-                <td><b>Gender</b></td>
-                <td><input name='gender' type='text'></td>
+                <td><b>Payment Status</b></td>
+                <td><input name='payment_status' type='text'></td>
 			</tr>
             <tr>
-                <td><b>Contact</b></td>
-                <td><input name='contact' type='text'></td>
-			</tr>
-            <tr>
-                <td><b>Address</b></td>
-                <td><input name='address' type='text'></td>
+                <td><b>Enrollment Status</b></td>
+                <td><input name='enrollment_status' type='text'></td>
 			</tr>
             <tr>
 				<td><input  type='submit' name='create_enrollment' value='Create'/></td>
@@ -57,28 +53,34 @@ session_start();
 		</table>
     </form>
     </center>";
-
-
+    /*  <td>".$id."</td>
+        <td>".$name."</td>
+        <td>".$num_units."</td>
+        <td>".$year."</td>
+        <td>".$sem."</td>
+        <td>".$pay."</td>
+        <td>".$enroll."</td>
+     */
 	//when form is submitted, get all values of each fields
 	if($_POST['create_enrollment']){
-		$name = $_POST['name'];
+		$sid = $_POST['name'];
 
 		include('../../connect.php');
 
-		$qry = "SELECT * from course where course_name = '{$name}'";
+		$qry = "SELECT * from Enrollment where student_id = '{$sid}'";
 		$result = $conn->query($qry);
-		$data = mysqli_fetch_array($result)['course_name'];
+		$data = mysqli_fetch_array($result)['student_id'];
 		
 
 		if($data){
-			echo "<center>Course name already exists!</center>" . $form;
+			echo "<center>Enrollment data for student already exists!</center>" . $form;
 		}else{
-			$qry = "SELECT count(*) as total_no from Course";
+			$qry = "SELECT count(*) as total_no from Enrollment";
 			$result = $conn->query($qry);
 			$id = mysqli_fetch_array($result)['total_no'];
 			$id++;
 			
-			$sql = "SELECT COUNT(*) as total FROM Course";
+			$sql = "SELECT count(*) as total FROM Enrollment";
 			$total = mysqli_fetch_array($conn->query($sql))['total'];
 			$total--;
 			
@@ -92,11 +94,11 @@ session_start();
 			} else {
 				$zero = null;
 			}
-			$course_id = 'CID-'.$zero.$id;
+			$enrolment_id = 'CID-'.$zero.$id;
 				
 
 		//insert values into the table
-			if(!mysqli_query($conn, "INSERT INTO Enrollment VALUES ('{$course_id}','{$name}')")){
+			if(!mysqli_query($conn, "INSERT INTO Enrollment VALUES ('{$enrollment_id}','{$sid}')")){
 				echo "Error description: " . mysqli_error($conn) . "<br> $form";
 			} else {
 				echo "<center>Successfully created a record! </center><br> $form";
